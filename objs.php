bab6bae02cc4d773a452258881f072cb0212b550
Cyrel Vandroth <carlstarr12345@gmail.com>
2011-10-03 11:18:19 -0400

    Fixed bugs. Public display/testing routine now in index.php. Host server doesn't have error catching set, so I'm actually looking at index.php through debug.php filter. Doh.

--- objs.php
+++ objs.php
@@ -12,21 +12,23 @@ class fleet{
 	// Several ships must belong to a fleet... sooo.. variable for list of ships? Array?
 	public $ships;
 	
+	// Constructor
 	public function __construct($in_name, $in_allegiance) {
 		$this->name = $in_name;
-		$this->allegiance = $in_allegiance
-		$this->location = 'La-la land'
-		$this->maximum_speed = 0.0
-		$this->commander = 0.0
+		$this->allegiance = $in_allegiance;
+		$this->location = 'La-la land';
+		$this->maximum_speed = 0.0;
+		$this->commander = 0.0;
 		$ships->ships = array();
 	}
+	
 	
 	// Add new ship (new class) to fleet: 
 	
 	
 	// Add new ship (stored class) to fleet: 
 	public function add_stored($in_name, $in_class){
-		// Load class from storage place
+		// Load ship class from storage place
 		// Create new ship object and define it with loaded class features? Call special constructor? Can you do that?
 	}
 	
@@ -34,44 +36,46 @@ class fleet{
 }
 
 class ship{
-	public $name
-	public $position
-	public $orientation
-	public $state
-	private $ship_class
-	private $size
-	private $shield_rating
-	private $hull
-	private $crew
-	private $troop
-	private $speed_sublight
-	private $speed_hyperspace
-	private $turn_rate
+	public $name;
+	public $position;
+	public $orientation;
+	public $state;
+	private $ship_class;
+	private $size;
+	private $shield_rating;
+	private $hull;
+	private $crew;
+	private $troop;
+	private $speed_sublight;
+	private $speed_hyperspace;
+	private $turn_rate;
 	// List of weapons? 
 	
+	// Constructor
 	public function __construct($in_name){
-		$this->name = $in_name
-		$this->position = array(0.0,0.0,0.0)
-		$this->state = 1
-		$this->orientation = 0.0
+		$this->name = $in_name;
+		$this->position = array(0.0,0.0,0.0);
+		$this->state = 1;
+		$this->orientation = 0.0;
 	}
 
 
 }
 
 class weapon{
-	public $name 
-	public $damage
-	public $fire_arc
-	public $spread
-	public $range
+	public $name; 
+	public $damage;
+	public $fire_arc;
+	public $spread;
+	public $range;
+	
 	
 	public function __construct($in_name, $in_damage, $in_fire_arc, $in_spread, $in_range){
-		$this->name = $in_name
-		$this->damage = $damage
-		$this->fire_arc = $in_fire_arc
-		$this->in_spread = $in_spread
-		$this->in_range = $in_range
+		$this->name = $in_name;
+		$this->damage = $damage;
+		$this->fire_arc = $in_fire_arc;
+		$this->in_spread = $in_spread;
+		$this->in_range = $in_range;
 	}
 
 
