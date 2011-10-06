bab8d0c4e658b6695dd6faa8b52913d71d9e5f42
Cyrel Vandroth <carlstarr12345@gmail.com>
2011-10-06 19:32:47 -0400

    realized each ship should be able to call a set ship_type, same with weapons..

--- objs.php
+++ objs.php
@@ -1,6 +1,24 @@
 <?php
 
-// debated on whether or not it was sensible to separate this into multiple files...
+
+class faction{
+	public $name;
+	public $starfighter_weapons_tech;
+	public $starship_weapons_tech;
+	public $maneuverability_tech;
+	public $shipspeed_tech;
+	public $defence_tech;
+	
+	public function __construct($in_name) {
+		$this->name = $in_name;
+		$this->starfighter_weapons_tech = 0;
+		$this->starship_weapons_tech = 0;
+		$this->maneuverability_tech = 0;
+		$this->shipspeed_tech = 0;
+		$this->defence_tech = 0;
+	}
+	
+}
 
 class fleet{
 	public $name;
@@ -22,6 +40,7 @@ class fleet{
 		$ships->ships = array();
 	}
 	
+	// WEEEEEEEEEEEE
 	
 	// Add new ship (new class) to fleet: 
 	
@@ -35,20 +54,14 @@ class fleet{
 	// Eventually include functionality for hyperdrive movement
 }
 
-class ship{
+class ship extends ship_type{
+	// instance of an idividual ship present in a fleet.
 	public $name;
 	public $position;
 	public $orientation;
 	public $state;
-	private $ship_class;
-	private $size;
-	private $shield_rating;
-	private $hull;
-	private $crew;
-	private $troop;
-	private $speed_sublight;
-	private $speed_hyperspace;
-	private $turn_rate;
+	
+
 	// List of weapons? 
 	
 	// Constructor
@@ -57,25 +70,53 @@ class ship{
 		$this->position = array(0.0,0.0,0.0);
 		$this->state = 1;
 		$this->orientation = 0.0;
+		
 	}
 
+
+}
+
+class ship_type{
+	public $ship_class;
+	public $size;
+	public $shield_rating;
+	public $hull;
+	public $crew;
+	public $troop;
+	public $speed_sublight;
+	public $speed_hyperspace;
+	public $turn_rate;
+	
+		public function __construct($in_ship_class, $in_size, $in_shield_rating, $in_hull, $in_crew, $in_troop, $in_speed_sublight, $in_speed_hyperspace, $in_turn_rate){
+			$this->ship_class = $in_ship_class;
+			$this->size = $in_size
+			$this->shield_rating = $in_shield_rating;
+			$this->hull = $in_hull;
+			$this->crew = $in_crew;
+			$this->troop = $in_troop;
+			$this->speed_sublight = $in_speed_sublight;
+			$this->speed_hyperspace = $in_speed_hyperspace;
+			$this->turn_rate = $in_turn_rate;
+	}
+}
+
+class weapon_group{
+	public $name; 
 
 }
 
 class weapon{
 	public $name; 
 	public $damage;
-	public $fire_arc;
-	public $spread;
 	public $range;
+	public $is_ion
 	
 	
-	public function __construct($in_name, $in_damage, $in_fire_arc, $in_spread, $in_range){
+	public function __construct($in_name, $in_damage, $in_range, $is_ion){
 		$this->name = $in_name;
 		$this->damage = $damage;
-		$this->fire_arc = $in_fire_arc;
-		$this->in_spread = $in_spread;
 		$this->in_range = $in_range;
+		$this->is_ion = $is_ion;
 	}
 
 
